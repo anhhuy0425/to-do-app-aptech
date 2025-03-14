1ccdcfe38255758200757dceb6bfc105aed169ba
Anh Huy <anhhuy.nguyendo@gmail.com>
2025-03-14 19:25:28 +0700

    session-1-route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/greeting', function () {
+    return ('Hello World');
+})->name('greeting');
+
+Route::get('/greeting/{name}', function ($name) {
+    return 'Hello ' . $name;
+});
+
+Route::get('/hiU', function () {
+    return redirect('/greeting');
+});
+
+Route::get('/hi2', function () {
+    return redirect()->route('greeting');
+});
+
+Route::get('/Home', function () {
+    return redirect('/');
+});
